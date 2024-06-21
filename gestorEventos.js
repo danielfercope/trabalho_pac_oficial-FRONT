7cd2742ce77fe94593918c0c70eddc1b3bc55ca7
José Mateus Loose <127354085+JMLoose@users.noreply.github.com>
2024-06-20 23:55:08 -0300

    Add files via upload

---
+++ gestorEventos.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const createEventTab = document.getElementById('create-event-tab');
+    const viewEventsTab = document.getElementById('view-events-tab');
+    const createEventForm = document.getElementById('create-event-form');
+    const viewEvents = document.getElementById('view-events');
+
+    createEventTab.addEventListener('click', function() {
+        createEventTab.classList.add('active');
+        viewEventsTab.classList.remove('active');
+        createEventForm.classList.remove('hidden');
+        viewEvents.classList.add('hidden');
+    });
+
+    viewEventsTab.addEventListener('click', function() {
+        viewEventsTab.classList.add('active');
+        createEventTab.classList.remove('active');
+        viewEvents.classList.remove('hidden');
+        createEventForm.classList.add('hidden');
+    });
+});
